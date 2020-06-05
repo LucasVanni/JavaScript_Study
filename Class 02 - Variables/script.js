b7baa52ecf5f4c622dcb6ce6aaef58f68022e211
Lucas Vanni <lucas.vanni@hotmail.com>
2020-06-05 10:32:35 -0300

    Second and third class JavaScript

---
+++ Class 02 - Variables/script.js
@@ -0,0 +1,24 @@
+var age = 90;
+
+alert(age);
+
+var nome = "Lucas";
+var sobrenome = "Vanni"
+
+alert(nome);
+
+var x = 10;
+
+var y = 15;
+
+var total = x + y;
+
+alert(total);
+
+var nomeCompleto = nome + " " + sobrenome;
+
+alert(nomeCompleto);
+
+
+
+
---
+++ Class 03 - Conditional/script.js
@@ -0,0 +1,9 @@
+var hora = 20;
+
+if ( hora > 7 && hora <= 12 ) {
+    console.log("Bom dia");
+} else if ( hora > 12 && hora <= 18 ) {
+    console.log("Boa Tarde");
+} else {
+    console.log("Boa noite");
+}
