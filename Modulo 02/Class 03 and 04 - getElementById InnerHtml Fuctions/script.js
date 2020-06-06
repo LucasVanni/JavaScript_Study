1aa3a8a08a6313fa2a48a3426a8633b27f36265b
Lucas Vanni <lucas.vanni@hotmail.com>
2020-06-06 19:05:05 -0300

    New class functions, native functions java script

--- Modulo 02/Class 03 and 04 - getElementById InnerHtml Fuctions/script.js
+++ Modulo 02/Class 03 and 04 - getElementById InnerHtml Fuctions/script.js
@@ -4,10 +4,33 @@ titulo.innerHTML = "Segundo Título";
 
 let campo = document.getElementById('campo');
 
-campo.placeholder = "Bonieky"
+campo.placeholder = "Nome do usuário";
 
 
 function alterar() {
     document.getElementById('titulo').innerHTML = "Paçoca";
     document.getElementById('campo').value = 'É uma delícia';
+}
+
+function alterarComProp(prop) {
+    document.getElementById('titulo').innerHTML = prop;
+    document.getElementById('campo').value = prop;
+}
+
+function somar(x, y) {
+    document.getElementById('campo').value = x + y;
+}
+
+function somarComRetorno(x, y) {
+    return x + y;
+}
+
+let result = somarComRetorno(10,15);
+
+console.log(result);
+
+function clicou(botao) {
+    document.getElementById('titulo').innerHTML = "Clicado"
+    botao.innerHTML = 'Clicou'
+    // alert("Você clicou no botão")
 }
