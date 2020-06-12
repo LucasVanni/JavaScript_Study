e8832a11e15a15e3047febea6c8e4bd2eca63dc2
Lucas Vanni <lucas.vanni@hotmail.com>
2020-06-12 15:56:46 -0300

    module 03 - Ainda Incompleto

--- Module 03/Array/script.js
+++ Module 03/Array/script.js
@@ -2,7 +2,22 @@ let lista = ['Ovo', 'Farinha', 'Corante', 'Massa'];
 
 let arrayToString = lista.toString();
 
-console.log(arrayToString);
 
 let res = lista.join('-');
-console.log(res)
+
+
+
+let numeros = [45, 4, 9, 16, 25];
+
+let usuarios = [
+    {id: 1, fistName: 'Lucas', lastName: 'Vanni'},
+    {id: 2, fistName: 'Fabio', lastName: 'Melo'},
+    {id: 3, fistName: 'Higor', lastName: 'Pacheco'},
+    {id: 4, fistName: 'Carlos', lastName: 'Vanni'}
+];
+
+let lista2 = [];
+
+    lista2 = usuarios.filter((item) => (item.lastName === 'Vanni'));
+
+console.log(lista2);
---
+++ Module 03/Dates/script.js
@@ -0,0 +1,7 @@
+let d = new Date()
+
+console.log(d.toDateString());
+
+console.log(d.toUTCString());
+
+console.log(d.toString())
