1512f60f2adad47e1acb66a313af5a069912ac36
Lucas Vanni <lucas.vanni@hotmail.com>
2020-06-14 03:26:55 -0300

    includes and repeats functions

---
+++ Module 03/Includes and Repeat/script.js
@@ -0,0 +1,15 @@
+let lista = ['Ovo', 'Farinha', 'Queijo', 'Batata', 'Macarrão'];
+
+console.log(lista.includes('Macarrão'));
+console.log(lista.includes('Peixe'));
+
+let nome = 'Lucas';
+
+console.log(nome.includes('a'));
+console.log(nome.includes('b'));
+
+console.log('-'.repeat(20));
+
+let name = 'Lucas ';
+
+console.log( name.repeat(5));
